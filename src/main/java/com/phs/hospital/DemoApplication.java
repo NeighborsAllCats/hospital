950926030d7280af881616ae31a18f649a26b2c9
maoty <846319617@qq.com>
2019-04-28 10:14:07 +0800

    hotfix:file loss fixed

--- src/main/java/com/phs/hospital/DemoApplication.java
+++ src/main/java/com/phs/hospital/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.phs.hospital;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan(basePackages = { "com.phs.hospital.core.common", "com.phs.hospital.core.service","com.phs.hospital.core.dal","com.phs.hospital.web"})
+@MapperScan(basePackages = {"com.phs.hospital.core.dal.dao"})
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class},scanBasePackages = "com.phs.hospital")
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/phs/hospital/core/common/enums/ErrorCode.java
@@ -0,0 +1,7 @@
+package com.phs.hospital.core.common.enums;
+
+public interface ErrorCode {
+    String getCode();
+
+    String getMessage();
+}
